eeed111fabf577c94255e848c43ac29d5ef13c41
dthanh <dthanhff318@gmail.com>
2024-12-04 12:01:19 +0700

    fix: fix lint when deploy

--- src/store/useGame.tsx
+++ src/store/useGame.tsx
@@ -2,7 +2,7 @@ import { create } from "zustand";
 
 const useGame = create((set) => ({
   gameList: [],
-  updateBears: (newBears) => set({ bears: newBears }),
+  updateBears: () => set({}),
 }));
 
 export default useGame;
