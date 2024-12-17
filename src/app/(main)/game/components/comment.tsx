46aa55f6b888727914039332d95c61f7bb96ef56
Your Name <youremail@example.com>
2024-12-17 23:01:58 +0700

    comment

---
+++ src/app/(main)/game/components/comment.tsx
@@ -0,0 +1,54 @@
+"use client";
+import * as React from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Textarea } from "@/components/ui/textarea";
+
+export function CommentGame() {
+  return (
+    <Card className="w-full">
+      <CardHeader>
+        <CardTitle className="text-xl">Comment</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" placeholder="Name of your project" />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" placeholder="Name of your project" />
+            </div>
+            <Textarea placeholder="Type your message here." />
+            <div className="flex items-center space-x-2">
+              <Checkbox id="terms" />
+              <label
+                htmlFor="terms"
+                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              >
+                I'd read and agree to the terms and conditions.
+              </label>
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <div></div>
+        <Button>Deploy</Button>
+      </CardFooter>
+    </Card>
+  );
+}
--- src/app/(main)/game/layout.tsx
+++ src/app/(main)/game/layout.tsx
@@ -3,6 +3,7 @@ import GameCard from "./components/game-card";
 import GameInstruction from "@/app/(main)/game/components/game-instruction";
 import ListGame, { TGame } from "@/listgame/list-game";
 import { ReactNode } from "react";
+import { CommentGame } from "./components/comment";
 
 type TProps = {
   children: ReactNode;
@@ -15,7 +16,7 @@ export default function GameLayout({ children }: TProps) {
         <div className="shrink-0 bg-red-300 w-full md:w-[220px] lg:w-[280px] xl:w-[330px] 2xl:w-[420px] h-[200px] sm:h-[300px] md:h-[610px]">
           Ads
         </div>
-        <div className="hidden xl:flex flex-col gap-4 justify-start h-[200px] sm:h-[300px] md:h-[610px] ">
+        <div className="hidden 2xl:flex flex-col gap-4 justify-start h-[200px] sm:h-[300px] md:h-[610px] ">
           {ListGame.map((game: TGame, index: number) => {
             if (index < 4) {
               return <GameCard key={index} game={game} />;
@@ -25,17 +26,18 @@ export default function GameLayout({ children }: TProps) {
         </div>
       </div>
       <div className="w-full flex flex-col md:flex-row mt-[50px] gap-4">
-        <div className="max-w-[1020px]">
-          <GameInstruction />
-        </div>
-        <div className="shrink-0 bg-white rounded-2xl md:w-[220px] lg:w-[280px] xl:w-[330px] 2xl:w-auto">
-          <div className="float-col p-[10px]">
+        <GameInstruction />
+        <div className="shrink-0 w-full md:w-[220px] lg:w-[280px] xl:w-[330px] 2xl:w-[420px]">
+          <div className="p-[10px] shrink-0 bg-white rounded-2xl">
             <h1 className="text-2xl ml-1 font-semibold mb-2">Hot Game</h1>
             <div className="grid grid-cols-4 sm:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-3 gap-[10px]">
-              {ListGame.map((game: TGame, index: number) => (
+              {ListGame.slice(0, 12).map((game: TGame, index: number) => (
                 <GameCard key={index} game={game} />
               ))}
             </div>
+          </div>
+          <div className="mt-[20px] float-col shrink-0 bg-white rounded-2xl">
+            <CommentGame></CommentGame>
           </div>
         </div>
       </div>
